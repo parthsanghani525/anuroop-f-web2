78b11f3f78042679bf50dfcad9544959398f6a4e
parthsanghani525 <81968386+parthsanghani525@users.noreply.github.com>
2022-08-10 11:33:18 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "e7d581d94e169b8166dddee4785e062c",
-"index.html": "cde9b5eea47dbc4e88d1de390000bc6b",
-"/": "cde9b5eea47dbc4e88d1de390000bc6b",
-"main.dart.js": "3f3e413754c82e7155bce847f89cd4ab",
+"index.html": "ef9ded447fde475c4793f77c01cd1f0b",
+"/": "ef9ded447fde475c4793f77c01cd1f0b",
+"main.dart.js": "3957d183f6b38e25e7415ae5860edf86",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "e6b3c830aeae573f91c8953d7e69db07",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
